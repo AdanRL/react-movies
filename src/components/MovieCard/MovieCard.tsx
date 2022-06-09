c128c3edc0fa129024770facafaf7b8c082678a2
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2022-06-09 22:56:47 +0100

    adding a start icon and tracking state ⭐

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react";
+import { FC, useState } from 'react';
 import { Link } from "react-router-dom";
+import { StarBorder, Star } from '@mui/icons-material';
 import styles from "./MovieCard.module.scss";
 
 interface Props {
@@ -12,20 +13,25 @@ interface Props {
 
 export const MovieCard: FC<Props> = ({id, title, movieImage, releaseDate, popularity = 0}) => {
   let movieSite = "/details/" + id.toString();
+  const [tracked, setTracked] = useState(false);
+  const handleTracked = () => {
+    setTracked(tracked ? false : true);
+    console.log(tracked);
+  };
   
   return(
-   
-    <Link to={movieSite} style={{textDecoration: "none"}}>
       <div className={styles.movieCard}>
-        <div className={styles.posterImage}>
-          <img src={movieImage} alt="Movie poster" />
-        </div>
+        {tracked ? <Star onClick={handleTracked} style={{ color: "yellow", transform: "translateX(85px)" }} /> : <StarBorder onClick={handleTracked} style={{ color: "yellow", transform: "translateX(85px)" }} />}
+        <Link to={movieSite} style={{textDecoration: "none"}}>
+          <div className={styles.posterImage}>
+            <img src={movieImage} alt="Movie poster" />
+          </div>
+        </Link>
         <div className={styles.infoContainer}>
           <div className={styles.title}>{title}</div>
           <div className={styles.date}>Release: {releaseDate}</div>
           <div className={styles.popularity}>Popularity: {popularity}</div>
         </div>
       </div>
-    </Link>
     );
 }
